41d93b1091522073ba1580ea9c51d260622094c0
Jana Traue <jana.traue@cyberus-technology.de>
2022-03-31 11:33:43 +0200

    Rename an internal variable.
    This is a preparation for ELF support which uses more generic names.

--- main.c
+++ main.c
@@ -110,57 +110,57 @@ void linux_boot(struct bzimage_params bzimage_params)
     char *linux_header_end = (char *)(bzimage_params.kernel_addr + 0x201);
     size_t linux_header_size = 0x202 + *linux_header_end - LINUX_HEADER_OFFSET;
 
-    struct linux_params *boot_params = malloc(sizeof(*boot_params));
-    memset(boot_params, 0, sizeof(*boot_params));
+    struct linux_params *linux_params = malloc(sizeof(*linux_params));
+    memset(linux_params, 0, sizeof(*linux_params));
 
-    die_on(LINUX_HEADER_OFFSET + linux_header_size > sizeof(*boot_params),
+    die_on(LINUX_HEADER_OFFSET + linux_header_size > sizeof(*linux_params),
            "Invalid linux header size");
 
-    memcpy((char *)boot_params + LINUX_HEADER_OFFSET,
+    memcpy((char *)linux_params + LINUX_HEADER_OFFSET,
            (const char *)(bzimage_params.kernel_addr + LINUX_HEADER_OFFSET),
            linux_header_size); // load header
 
-    die_on(boot_params->param_block_version < 0x0205,
+    die_on(linux_params->param_block_version < 0x0205,
            "Kernel does not support boot protocol >= 2.05\n");
     die_on(
-        !boot_params->relocatable_kernel,
+        !linux_params->relocatable_kernel,
         "Kernel is not relocatable. The Linux kernel must be built with CONFIG_RELOCATABLE=y\n");
-    die_on(bzimage_params.kernel_addr % boot_params->kernel_alignment != 0,
-           "Kernel needs to be aligned at 0x%x\n", boot_params->kernel_alignment);
+    die_on(bzimage_params.kernel_addr % linux_params->kernel_alignment != 0,
+           "Kernel needs to be aligned at 0x%x\n", linux_params->kernel_alignment);
 
     printf("Setting up E820 map\n");
     // Coreboot already obtained the memory map. Simply copy it over into the kernel params.
-    boot_params->e820_map_nr = lib_sysinfo.n_memranges;
+    linux_params->e820_map_nr = lib_sysinfo.n_memranges;
     for (int i = 0; i < lib_sysinfo.n_memranges; i++) {
-        boot_params->e820_map[i].addr = lib_sysinfo.memrange[i].base;
-        boot_params->e820_map[i].size = lib_sysinfo.memrange[i].size;
-        boot_params->e820_map[i].type = lib_sysinfo.memrange[i].type;
+        linux_params->e820_map[i].addr = lib_sysinfo.memrange[i].base;
+        linux_params->e820_map[i].size = lib_sysinfo.memrange[i].size;
+        linux_params->e820_map[i].type = lib_sysinfo.memrange[i].type;
     }
 
     // There is a maximum size for the command line, which could be obtained from the kernel
     // header if the boot protocol version is >= 2.06. We don't check anything here and
     // simply assume the user knows what they are doing.
     if (bzimage_params.cmdline_addr != 0) {
-        boot_params->cmd_line_ptr = bzimage_params.cmdline_addr;
+        linux_params->cmd_line_ptr = bzimage_params.cmdline_addr;
     }
 
     if (bzimage_params.initrd_addr != 0) {
-        boot_params->initrd_start = bzimage_params.initrd_addr;
-        boot_params->initrd_size = *(u32 *)bzimage_params.initrd_size_addr;
+        linux_params->initrd_start = bzimage_params.initrd_addr;
+        linux_params->initrd_size = *(u32 *)bzimage_params.initrd_size_addr;
 
-        die_on(boot_params->initrd_start + boot_params->initrd_size
-                   > boot_params->initrd_addr_max,
+        die_on(linux_params->initrd_start + linux_params->initrd_size
+                   > linux_params->initrd_addr_max,
                "Initrd area exceeds maximum initrd address as required by the kernel\n");
     }
 
     const uint8_t custom_loader_type = 0xFF;
-    boot_params->loader_type = custom_loader_type;
+    linux_params->loader_type = custom_loader_type;
 
     // According to the Linux boot protocol, we should set up a simple GDT here. Both 32-bit
     // and 64-bit Linux set their own GDT right after the entry point, and simply jumping to
     // the entry address works without any issues so far, so we're skipping this.
 
-    uint8_t setup_sects = boot_params->setup_hdr;
+    uint8_t setup_sects = linux_params->setup_hdr;
 
     if (setup_sects == 0) {
         setup_sects = 4;
@@ -173,8 +173,8 @@ void linux_boot(struct bzimage_params bzimage_params)
     die_on(entry_ptr_32bit < bzimage_params.kernel_addr,
            "32-bit entry pointer lies beyond 4G\n");
 
-    asm volatile("jmp *%[entry_ptr];" ::[entry_ptr] "r"(entry_ptr_32bit), "S"(boot_params),
-                 "m"(boot_params));
+    asm volatile("jmp *%[entry_ptr];" ::[entry_ptr] "r"(entry_ptr_32bit), "S"(linux_params),
+                 "m"(linux_params));
 
     __builtin_unreachable();
 }
