52c628a8a7abc988231e8e23b02b7cede2073d3b
Jana Traue <jana.traue@cyberus-technology.de>
2022-03-31 11:32:03 +0200

    Verify that a Linux kernel is passed.
    The loader will soon get support for booting ELF binaries. This is a
    preparation that enables the loader to detect whether a Linux kernel or
    an ELF is passed.

--- main.c
+++ main.c
@@ -28,10 +28,49 @@ struct bzimage_params {
     size_t cmdline_addr;
 };
 
-static struct bzimage_params initialize_from_fw_cfg()
+enum boot_protocol {
+    UNKNOWN,
+    LINUX,
+};
+
+static struct bzimage_params get_boot_params_from_fw_cfg();
+static enum boot_protocol get_boot_protocol(struct bzimage_params);
+void linux_boot(struct bzimage_params);
+
+int main(void)
 {
-    die_on(!fw_cfg_present(), "Firmware config device not found\n");
-    struct bzimage_params params;
+    struct bzimage_params params = get_boot_params_from_fw_cfg();
+    enum boot_protocol boot_protocol = get_boot_protocol(params);
+
+    switch (boot_protocol) {
+    case LINUX:
+        linux_boot(params);
+        break;
+    default:
+        die_on(true, "Failed to recognize kernel file format. Supported format is bzImage.\n");
+    }
+}
+
+// Identify the type of the kernel.
+static enum boot_protocol get_boot_protocol(struct bzimage_params params)
+{
+    size_t bzimage_addr = params.bzimage_addr;
+
+    die_on(bzimage_addr == 0, "Did not find the address of kernel.\n");
+
+    // Linux kernel supporting the "new" boot protocol have a magic number at a specific offset.
+    // See https://www.kernel.org/doc/Documentation/x86/boot.txt.
+    if (memcmp((uint32_t *)(bzimage_addr + 0x202), "HdrS", 4) == 0) {
+        return LINUX;
+    }
+
+    return UNKNOWN;
+}
+
+static struct bzimage_params get_boot_params_from_fw_cfg()
+{
+    struct bzimage_params params = {
+        .bzimage_addr = 0, .initrd_addr = 0, .initrd_size_addr = 0, .cmdline_addr = 0};
 
     uint16_t selector;
 
@@ -54,16 +93,17 @@ static struct bzimage_params initialize_from_fw_cfg()
     if (selector != 0) {
         fw_cfg_get(selector, &params.cmdline_addr, sizeof(params.cmdline_addr));
     }
-
     return params;
 }
 
-int main(void)
+// Linux boot follows the Linux x86 32-bit Boot Protocol
+// (https://www.kernel.org/doc/html/latest/x86/boot.html#bit-boot-protocol).
+void linux_boot(struct bzimage_params bzimage_params)
 {
-    struct bzimage_params bzimage_params = initialize_from_fw_cfg();
-
-    die_on(bzimage_params.bzimage_addr == 0, "bzImage start address not found!\n"
-           "The VMM does not offer a Linux kernel via fw-cfg. Is the relevant config option missing?\n");
+    die_on(
+        bzimage_params.bzimage_addr == 0,
+        "bzImage start address not found!\n"
+        "The VMM does not offer a Linux kernel via fw-cfg. Is the relevant config option missing?\n");
 
     printf("Loading Linux kernel from address: 0x%lx\n", bzimage_params.bzimage_addr);
 
@@ -77,7 +117,7 @@ int main(void)
            "Invalid linux header size");
 
     memcpy((char *)boot_params + LINUX_HEADER_OFFSET,
-           (char *)(bzimage_params.bzimage_addr + LINUX_HEADER_OFFSET),
+           (const char *)(bzimage_params.bzimage_addr + LINUX_HEADER_OFFSET),
            linux_header_size); // load header
 
     die_on(boot_params->param_block_version < 0x0205,
@@ -131,7 +171,7 @@ int main(void)
     // An overflowing unsigned integer addition will simply wrap. Such an overflow can be
     // detected by checking whether the result is smaller than one of the original values.
     die_on(entry_ptr_32bit < bzimage_params.bzimage_addr,
-           "32bit entry pointer lies beyond 4G\n");
+           "32-bit entry pointer lies beyond 4G\n");
 
     asm volatile("jmp *%[entry_ptr];" ::[entry_ptr] "r"(entry_ptr_32bit), "S"(boot_params),
                  "m"(boot_params));
